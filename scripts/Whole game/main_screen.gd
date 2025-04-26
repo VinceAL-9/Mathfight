45e7ec34913b85d8f3a6dce2b422849a26f929e9
Vince <vinceamiel.latabe-24@cpu.edu.ph>
2025-04-26 12:07:18 +0800

    Minor updates on path and animation
    Changed the path of menu login in code, made the enemy_attack animation smoother

--- scripts/Whole game/main_screen.gd
+++ scripts/Whole game/main_screen.gd
@@ -1,4 +1,4 @@
 extends Control
 
 func _on_button_button_down() -> void:
-	get_tree().change_scene_to_file("res://scenes/Whole game/Login/menu_login.tscn")
+	get_tree().change_scene_to_file("res://scenes/Whole game/menu_login.tscn")
