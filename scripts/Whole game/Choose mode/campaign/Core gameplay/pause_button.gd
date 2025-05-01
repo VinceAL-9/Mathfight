e1448ee911d85cb38165d5382a7f36aa4bdb58e1
johquendo <208431245+johquendo@users.noreply.github.com>
2025-05-01 20:48:20 +0800

    Adjustments to the match results scene, added features
    Added the numbers for the statistics of the match results, made the button for continue and retry functional

--- scripts/Whole game/Choose mode/campaign/Core gameplay/pause_button.gd
+++ scripts/Whole game/Choose mode/campaign/Core gameplay/pause_button.gd
@@ -13,6 +13,7 @@ func paused():
 
 func _on_exit_button_pressed() -> void:
 	resume()
+	Leveldata.level = 0
 	Transition.play("fade_out")
 	await get_tree().create_timer(1).timeout
 	get_tree().change_scene_to_file("res://scenes/Whole game/Choose mode/campaign/campaign_select.tscn")
