320576a67d2a922f597e15629419739058478d6c
johquendo <208431245+johquendo@users.noreply.github.com>
2025-05-01 21:15:10 +0800

    Adjusted code to fix conflicted

--- scripts/Whole game/Choose mode/campaign/match_results.gd
+++ scripts/Whole game/Choose mode/campaign/match_results.gd
@@ -27,6 +27,7 @@ func _on_back_button_pressed() -> void:
 	Functions.load_screen_to_scene("res://scenes/Whole game/main_menu.tscn")
 
 func _on_continue_button_pressed() -> void:
+	Music.play_menu_music()
 	if Leveldata.player_win:
 		if Leveldata.level == 1:
 			Functions.load_screen_to_scene("res://scenes/Whole game/Choose mode/campaign/levels/level_2.tscn")
