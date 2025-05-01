5f335330ddf6d9a39d142aad176ce3e7a8bb3821
johquendo <208431245+johquendo@users.noreply.github.com>
2025-05-01 18:09:19 +0800

    Fixed animation in match results scene

--- scripts/Whole game/Choose mode/campaign/match_results.gd
+++ scripts/Whole game/Choose mode/campaign/match_results.gd
@@ -1,7 +1,10 @@
 extends Control
 
+@onready var animated_sprite_2d = $CanvasLayer/AnimatedSprite2D
+
 func _ready():
-	$CanvasLayer/AnimatedSprite2D.play("idle")
+	animated_sprite_2d.play("idle")
+
 func _on_back_button_pressed() -> void:
 	Functions.load_screen_to_scene("res://scenes/Whole game/main_menu.tscn")
 
