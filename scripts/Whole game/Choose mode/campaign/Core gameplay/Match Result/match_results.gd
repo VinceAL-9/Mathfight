c5f46a89b26256ada799cfec5b5e736a1086b007
johquendo <208431245+johquendo@users.noreply.github.com>
2025-05-06 07:59:43 +0800

    Match results comments and some adjustments
    Fixed sprite container outline disappearing with button, and music with match results retry and continue button.

--- scripts/Whole game/Choose mode/campaign/Core gameplay/Match Result/match_results.gd
+++ scripts/Whole game/Choose mode/campaign/Core gameplay/Match Result/match_results.gd
@@ -10,26 +10,28 @@ extends Control
 func _ready():
 	transition.play("fade_in")
 	animated_sprite_2d.play("idle")
+	# statistics of the match results gotten from the level data global script
 	time_elapsed.bbcode_text = str(fmod(Leveldata.time_elapsed, 3600) / 60).pad_decimals(0)  + ":" + str(fmod(Leveldata.time_elapsed, 60)).pad_decimals(0)
 	items_solved.bbcode_text = Leveldata.items_solved
 	items_not_solved.bbcode_text = Leveldata.items_not_solved
+	# resets the data in the level data global script
 	Leveldata.time_elapsed = 0.0
 	Leveldata.items_solved = 0
 	Leveldata.items_not_solved = 0
-	if Leveldata.level == 3:
+	if Leveldata.level == 3 and Leveldata.player_win: # if level is level 3 and has won, disable button
 		button_play.visible = false
-	if !Leveldata.player_win:
+	if !Leveldata.player_win: # if player loses, the button text is retry instead of continue
 		button_play.text = "Retry"
 	
 
-func _on_back_button_pressed() -> void:
+func _on_back_button_pressed() -> void: # back to main menu button
 	Music.play_menu_music()
 	
-	if Leveldata.player_win:
-		if Leveldata.level == 1:
-			if 2 not in Gamestate.unlocked_levels:
-				Gamestate.unlocked_levels.append(2)
-			Functions.load_screen_to_scene("res://scenes/Whole game/main_menu.tscn")
+	if Leveldata.player_win: # checks if player has won
+		if Leveldata.level == 1: # checks what level 
+			if 2 not in Gamestate.unlocked_levels: # checks if next level is in the levels unlocked
+				Gamestate.unlocked_levels.append(2) # adds the level to the levels unlocked
+			Functions.load_screen_to_scene("res://scenes/Whole game/main_menu.tscn") # back to main menu
 		elif Leveldata.level == 2:
 				if 3 not in Gamestate.unlocked_levels:
 					Gamestate.unlocked_levels.append(3)
@@ -40,16 +42,17 @@ func _on_back_button_pressed() -> void:
 		Leveldata.level = 0
 		Functions.load_screen_to_scene("res://scenes/Whole game/main_menu.tscn")
 
-func _on_continue_button_pressed() -> void:
-	Music.play_pregame()
+func _on_continue_button_pressed() -> void: # continue
 	
-	if Leveldata.player_win:
-		if Leveldata.level == 1:
-			if 2 not in Gamestate.unlocked_levels:
-				Gamestate.unlocked_levels.append(2)
-			Gamestate.selected_level = 2
-			Functions.load_screen_to_scene("res://scenes/Whole game/Choose mode/campaign/levels/Pre-game2.tscn")
-			Leveldata.level = 2
+	
+	if Leveldata.player_win: # checks if player has won
+		Music.play_pregame()
+		if Leveldata.level == 1: # checks what level
+			if 2 not in Gamestate.unlocked_levels: # checks if next level is in the levels unlocked  
+				Gamestate.unlocked_levels.append(2) # adds the level to the levels unlocked
+			Gamestate.selected_level = 2 # changes selected level
+			Functions.load_screen_to_scene("res://scenes/Whole game/Choose mode/campaign/levels/Pre-game2.tscn") # changes scene to pregame 2
+			Leveldata.level = 2 # sets level in level data to 2
 
 		elif Leveldata.level == 2:
 			if 3 not in Gamestate.unlocked_levels:
@@ -60,10 +63,15 @@ func _on_continue_button_pressed() -> void:
 
 		elif Leveldata.level == 3:
 			Functions.load_screen_to_scene("res://scenes/Whole game/main_menu.tscn")
-	else:
+	else: # for retry
 		if Leveldata.level == 1:
+			Music.play_level_1()
 			Functions.load_screen_to_scene("res://scenes/Whole game/Choose mode/campaign/levels/level_1.tscn")
 		elif Leveldata.level == 2:
+			Music.play_level_2()
 			Functions.load_screen_to_scene("res://scenes/Whole game/Choose mode/campaign/levels/level_2.tscn")
+		elif Leveldata.level == 3:
+			Music.play_level_3()
+			Functions.load_screen_to_scene("res://scenes/Whole game/Choose mode/campaign/levels/level_3.tscn")
 		
-		
+	
